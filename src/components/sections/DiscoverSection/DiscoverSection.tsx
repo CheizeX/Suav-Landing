78fa7f8cd227ff56a1a9a98109cd66f2608f912f
Ezequiel Rivas <ezequiel@innovit.cl>
2022-08-08 23:36:10 -0300

    feat: advantages

--- src/components/sections/DiscoverSection/DiscoverSection.tsx
+++ src/components/sections/DiscoverSection/DiscoverSection.tsx
@@ -41,7 +41,7 @@ export const DiscoverSection: FC = () => {
     <section className="min-h-[900px] w-full max-w-[1366px] flex flex-col justify-between items-center overflow-x-hidden py-14">
       <div className="w-full ml-[54px] ">
         <h1 className="text-[35px] text-gray_5 font-semibold">
-          Discover Stylist on Suav
+          Discover stylists on Suav
         </h1>
       </div>
       <StyledSlider
